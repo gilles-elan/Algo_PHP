9b4224e00ac221d2f79e792231dac02a5b850358
Gilles_Elan <gilles.muess@elan-formation.fr>
2021-01-25 13:32:21 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+  // Ceci est un commentaire sur une ligne
+  $maVariable = "Coucou tout le monde";
+
+  /*
+    Ceci est un bloc de commentaire
+  */
+  echo "<p>".$maVariable."</p>";
+  print_r($maVariable);
+  var_dump($maVariable);
+?>
